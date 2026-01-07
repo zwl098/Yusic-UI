cbf8ab7504026a50dd93006819e68e8171212dcb
zhangwenling <2570104770@qq.com>
2026-01-07 18:05:21 +0800

    feat: Add Vant UI library and configure automatic component import.

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from '@vant/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
